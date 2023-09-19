7fd71b1231011698c104dfe23ed6edf97629659d
Ariel Kwak <anniekwak123@gmail.com>
2023-09-19 16:01:23 -0400

    Add multiply signs to work

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -33,5 +33,19 @@ export default function QueryProcessor(query: string): string {
     }
   }
 
+  if (query.toLowerCase().includes("multiply")){
+    const queryString = query.toLowerCase();
+    const numbers = queryString.match(/\d+/g);
+
+    if (numbers && numbers.length >= 2) {
+      const num1 = parseInt(numbers[0]);
+      const num2 = parseInt(numbers[1]);
+      return "" + (num1 * num2);
+    } else {
+      // Handle the case where two numbers couldn't be extracted.
+      return "Could not extract two numbers from the query.";
+    }
+  }
+
   return "";
 }
